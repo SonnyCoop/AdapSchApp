0736d3edf005fcecb07bacb3272f5a43a33ecd2c
Sonny Cooper <sonnykidd@icloud.com>
2023-09-03 12:19:59 +0100

    added swift file

---
+++ AdapSchApp/AdapSchApp/AdapSchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdapSchAppApp.swift
+//  AdapSchApp
+//
+//  Created by Sonny Cooper on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdapSchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
